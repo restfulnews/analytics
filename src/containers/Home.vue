ade3c2dab25c7e1c91b4b8f279d64930f51b9401
Chris Johnston <chris14b@gmail.com>
2018-04-26 23:12:17 +1000

    Added a protoype new home page, and a subsequent refine page. Not fully working yet, but gives an idea of how things will work in the future.

---
+++ src/containers/Home.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <span class="md-display-4">Welcome</span>
+        <md-card class="md-accent" md-with-hover>
+            <md-card-header>
+                <div class="md-title">Analyse the news</div>
+            </md-card-header>
+
+            <md-card-content>
+                <span>I'm interested in how...</span>
+                <md-chips
+                    md-deletable
+                    v-model="keywords"
+                    md-check-duplicated="true"
+                    md-placeholder="keywords"
+                />
+                <span>...affects...</span>
+                <md-chips
+                    v-model="tickers"
+                    md-check-duplicated="true"
+                    md-placeholder="companies and company tickers"
+                />
+            </md-card-content>
+
+            <md-card-actions>
+                <router-link :to="{ name: 'Refine', params: { keywords: keywords, tickers: tickers } }" tag="md-button">Refine</router-link>
+            </md-card-actions>
+        </md-card>
+        <md-progress-bar class="loading" md-mode="indeterminate"
+        v-if="getSearchStatus == 'fetching'"/>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Home',
+  computed: {
+    keywords: {
+      get() { return this.$store.state.search.keywords; },
+      set(val) { this.updateSearchKeywords(val); },
+    },
+    tickers: {
+      get() { return this.$store.state.search.tickers; },
+      set(val) { this.updateSearchTickers(val); },
+    },
+  },
+  methods: {
+    ...mapActions([
+      'updateSearchKeywords',
+      'updateSearchTickers',
+      'updateAppTitle',
+    ]),
+  },
+  mounted() {
+    this.updateAppTitle('Home');
+  },
+};
+</script>
+
+<style scoped>
+.news-search {
+  padding: 1em;
+}
+.news-card {
+  background-color: white;
+  margin-top: 2em;
+}
+.empty-state {
+  background-color: rgb(240, 240, 240);
+  margin-top: 20%;
+}
+.chips {
+  display: inline;
+  padding-bottom: .4em;
+  background-color: white;
+}
+.loading {
+  text-align: center;
+  width: 90%;
+  margin: 25px;
+}
+</style>
---
+++ src/containers/Refine.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="columns is-fullheight">
+    <div class="column is-5 is-sidebar-menu">
+      <md-card class="news-search">
+        <div class="md-layout">
+          <div class="md-layout-item">
+            <md-datepicker v-model="startTime">
+              <label>Start time</label>
+            </md-datepicker>
+          </div>
+          <div class="md-layout-item">
+            <md-datepicker v-model="endTime">
+              <label>End time</label>
+            </md-datepicker>
+          </div>
+        </div>
+        <md-button
+          class="md-raised md-primary search-btn"
+          @click="updateSearchResults()"
+        >
+          Search News
+        </md-button>
+      </md-card>
+      <md-progress-bar class="loading" md-mode="indeterminate"
+        v-if="getSearchStatus == 'fetching'"/>
+      <news-card
+        v-for="result in getSearchResults"
+        v-bind:key="result.fingerprint"
+        v-bind:thumbnail="result.thumbnail"
+        v-bind:abstract="result.abstract"
+        v-bind:publishedAt="result.publishedAt"
+        v-bind:title="result.title"
+        v-bind:url="result.url"
+      />
+    </div>
+    <div class="column is-main-content is-hidden-mobile">
+      <graph-card
+        v-bind:tickers="tickers"
+        v-bind:results="getSearchResults"
+      />
+    </div>
+    <auth />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+// import Auth from '@/containers/Auth';
+import NewsCard from '@/components/NewsCard';
+import GraphCard from '@/components/GraphCard';
+import FeatureUnavailable from '@/components/FeatureUnavailable';
+import router from '../router'
+
+export default {
+  name: 'Explore',
+  data () {
+        return {
+            keywords: 'something went wrong lol oops',
+            tickers: 'tick tock im a clock',
+        }
+    },
+  created() {
+      this.keywords = this.$route.params.keywords;
+      this.tickers = this.$route.params.tickers;
+  },
+  components: {
+    // auth: Auth,
+    'news-card': NewsCard,
+    'graph-card': GraphCard,
+    'feature-unavailable': FeatureUnavailable,
+  },
+  computed: {
+    ...mapGetters([
+      'getSearchResults',
+      'getSearchStatus',
+    ]),
+    startTime: {
+      get() { return this.$store.state.search.startTime; },
+      set(val) { this.updateSearchStartTime(val); },     
+    },
+    endTime: {
+      get() { return this.$store.state.search.endTime; },
+      set(val) { this.updateSearchEndTime(val); },
+    },
+    keywords: {
+      get() { return this.$store.state.search.keywords; },
+      set(val) { this.updateSearchKeywords(val); },
+    },
+    tickers: {
+      get() { return this.$store.state.search.tickers; },
+      set(val) { this.updateSearchTickers(val); },
+    },
+  },
+  methods: {
+    ...mapActions([
+      'updateSearchStartTime',
+      'updateSearchEndTime',
+      'updateSearchKeywords',
+      'updateSearchTickers',
+      'updateSearchResults',
+      'updateAppTitle',
+    ]),
+    navigate() {
+            router.go(-1);
+        },
+  },
+  mounted() {
+    this.updateAppTitle('Explore');
+  },
+};
+</script>
+
+<style scoped>
+.columns.is-fullheight {
+  min-height: calc(96vh - ( 3.25rem - .75rem ));
+  max-height: calc(96vh - ( 3.25rem - .75rem ));
+  display: flex;
+  flex-direction: row;
+  justify-content: stretch;
+}
+.columns.is-fullheight .column {
+  overflow-y: auto;
+}
+.news-search {
+  padding: 1em;
+}
+.news-card {
+  background-color: white;
+  margin-top: 2em;
+}
+.empty-state {
+  background-color: rgb(240, 240, 240);
+  margin-top: 20%;
+}
+.chips {
+  display: inline;
+  padding-bottom: .4em;
+}
+.loading {
+  text-align: center;
+  width: 90%;
+  margin: 25px;
+}
+</style>
